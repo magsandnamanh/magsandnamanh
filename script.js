5f09e90eaeb253548987956a5dad56301d17c399
Namanh Tran <NamanhDTran@gmail.com>
2024-08-20 20:55:34 -0700

    add layout and picture text not finalized yet

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function countDown() {
     + String(minutes).padStart(2, '0') + ":" + String(seconds).padStart(2, '0');
 
     // If the count down is finished, write some text
-    if (distance < 0) {
+    if (distance > 0) {
       clearInterval(x);
       document.getElementById("count-down-container").style.display = "none";
       showAfterCountDown()
